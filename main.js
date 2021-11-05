aae1ddfc5a7f9a93571da313dd88d6e7cb3b038c
eshanajain <82397896+eshanajain@users.noreply.github.com>
2021-11-05 10:25:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-WebKitCSSMatrix.set({
+Webcam.set({
     width:350,
     height:300,
     image_format : 'png',
@@ -18,3 +18,23 @@ function take_snapshot()
 
 console.log('ml5 version:', ml5.version);
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/zdaVHvPZD/',modelLoaded);
+
+function modelLoaded(){
+    console.log('model loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('capture_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
