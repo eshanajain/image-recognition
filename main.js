932e25172b2af57c16e0789c0fe855d76d713f35
eshanajain <82397896+eshanajain@users.noreply.github.com>
2021-11-06 11:50:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function take_snapshot()
 }
 
 console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/zdaVHvPZD/',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/zdaVHvPZD/model.json',modelLoaded);
 
 function modelLoaded(){
     console.log('model loaded!');
